f94f99a3c234aead97227a577ee1a119104fd67d
NovelaxNeko <42329190+NovelaxNeko@users.noreply.github.com>
2025-08-11 18:14:19 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'NXP'
+description 'NXP Doortuning'
+version '1.0.0'
+
+license 'MIT'
+
+replace_level_meta 'gta5'
+
+files {
+    'gta5.meta',
+    'doortuning.ymt'
+}
